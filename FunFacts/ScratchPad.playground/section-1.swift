0a5aa2a69b35f9d0f493ea93867d7aa438eb08d8
Alberto Jauregui <bastertwo@gmail.com>
2014-12-20 23:52:14 -0700

    Adding a playground file to the project

---
+++ FunFacts/ScratchPad.playground/section-1.swift
@@ -0,0 +1,15 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//var fruitsArray : [String] = ["apples"]
+
+var fruitsArray = ["apples"]
+fruitsArray.append("bananas")
+fruitsArray
+
+// optional
+var optionalString : String? = "hello"
+optionalString = nil
